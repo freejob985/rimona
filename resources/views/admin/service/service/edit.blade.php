b1d4f58785e23c17713b110435df4bf9f2e2e124
freejob985 <freejob985@gmail.com>
2021-01-16 01:57:28 +0200

    asd

--- resources/views/admin/service/service/edit.blade.php
+++ resources/views/admin/service/service/edit.blade.php
@@ -301,14 +301,36 @@
             $("#Logo").click(function () {
 
               
-                $("#Logo_ar").append("<div class=\"form-group\">\n" +
-                    "        <textarea  placeholder=\"Please enter data  Title\" name=\"Title\"   style=\"resize: none;font-size: 18px;font-weight: 600;color: white !important;background: #0e1726;\"class=\"form-control\" rows=\"5\" id=\"comment\"></textarea>\n" +
-                    "        @if ($errors->has('Title'))\n" +
-                    "        <span class=\"helper-text\" data-error=\"wrong\" data-success=\"right\">{{ $errors->first('Title') }}</span>\n" +
-                    "        @endif\n" +
-                    "      </div>
-                     <div class=\"custom-file\"> <input type=file name=\"Logo[]\" ">+\n' +
-                '<label >Choose Images</label>");
+                $("#Logo_ar").append("<div class=\"form-row\">\n" +
+                    "    <div class=\"col-md-12 mb-4\">\n" +
+                    "      <label for=\"Title'\">Title'</label>\n" +
+                    "      <input type=\"text\"  placeholder=\"Please enter data\"   class=\"form-control\"   name=\"Title'\"  id=\"Title'\" placeholder=\"\" value=\"{{Request::old('Title'')}}\" >\n" +
+                    "      @if ($errors->has('Title''))\n" +
+                    "      <div class=\"invalid-feedback\">\n" +
+                    "        {{ $errors->first('Title'') }}\n" +
+                    "      </div>\n" +
+                    "      @endif\n" +
+                    "    </div>\n" +
+                    "  </div>\n" +
+                    "\n" +
+                    "\n" +
+                    "  <div class=\"form-group\">\n" +
+                    "    <textarea  placeholder=\"Please enter data  paragraph\" name=\"paragraph\"   style=\"resize: none;font-size: 18px;font-weight: 600;color: white !important;background: #0e1726;\"class=\"form-control\" rows=\"5\" id=\"comment\"></textarea>\n" +
+                    "    @if ($errors->has('explanation'))\n" +
+                    "    <span class=\"helper-text\" data-error=\"wrong\" data-success=\"right\">{{ $errors->first('paragraph') }}</span>\n" +
+                    "    @endif\n" +
+                    "  </div>\n" +
+                    "\n" +
+                    "  <div class=\"custom-file-container\" data-upload-id=\"myFirstImage\">\n" +
+                    "    <label>Upload (Single File) <a href=\"javascript:void(0)\" class=\"custom-file-container__image-clear\"\n" +
+                    "        title=\"Clear Image\">x</a></label>\n" +
+                    "    <label class=\"custom-file-container__custom-file\">\n" +
+                    "      <input type=\"file\"  name=\"File\"  class=\"custom-file-container__custom-file__custom-file-input\" accept=\"image/*\">\n" +
+                    "      <input type=\"hidden\" value=\"10485760\" />\n" +
+                    "      <span class=\"custom-file-container__custom-file__custom-file-control\"></span>\n" +
+                    "    </label>\n" +
+                    "    <div class=\"custom-file-container__image-preview\"></div>\n" +
+                    "  </div>");
               //  return false;
 
 
@@ -319,25 +341,15 @@
               
 
 
-                $("#Products_ar").append(" <div class=\"form-group\">\n" +
-                    "        <textarea  placeholder=\"Please enter data  Title\" name=\"Title\"   style=\"resize: none;font-size: 18px;font-weight: 600;color: white !important;background: #0e1726;\"class=\"form-control\" rows=\"5\" id=\"comment\"></textarea>\n" +
-                    "        @if ($errors->has('Title'))\n" +
-                    "        <span class=\"helper-text\" data-error=\"wrong\" data-success=\"right\">{{ $errors->first('Title') }}</span>\n" +
-                    "        @endif\n" +
-                    "      </div> <div class=\"custom-file\"> <input type=file name=\"Products[]\" ">+\n' +
-                    '<label >Choose Images</label>");
+                $("#Products_ar").append(' <div class=\"custom-file\"> <input type=file name=\"Products[]\" ">+\n' +
+                    '<label >Choose Images</label>');
               //  return false;
             });
 
             $("#Projects").click(function () {
                 
-                $("#Projects_ar").append(" <div class=\"form-group\">\n" +
-                    "        <textarea  placeholder=\"Please enter data  Title\" name=\"Title\"   style=\"resize: none;font-size: 18px;font-weight: 600;color: white !important;background: #0e1726;\"class=\"form-control\" rows=\"5\" id=\"comment\"></textarea>\n" +
-                    "        @if ($errors->has('Title'))\n" +
-                    "        <span class=\"helper-text\" data-error=\"wrong\" data-success=\"right\">{{ $errors->first('Title') }}</span>\n" +
-                    "        @endif\n" +
-                    "      </div> <div class=\"custom-file\"> <input type=file name=\"Projects[]\" ">+\n' +
-                    '<label >Choose Images</label>");
+                $("#Projects_ar").append(' <div class=\"custom-file\"> <input type=file name=\"Projects[]\" ">+\n' +
+                    '<label >Choose Images</label>');
 
               //  return false;
             });
