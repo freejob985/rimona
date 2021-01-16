bd23fa8cedeef00edea7b61f0d04e85cd468253c
freejob985 <freejob985@gmail.com>
2021-01-16 12:44:30 +0200

    asd

--- resources/views/admin/service/service/edit.blade.php
+++ resources/views/admin/service/service/edit.blade.php
@@ -237,8 +237,8 @@
 
                               <div class="form-row">
                                 <div class="col-md-12 mb-4">
-                                  <label for="Title'">Title'</label>
-                                  <input type="text"  placeholder="Please enter data"   class="form-control"   name="Title"  id="Title" placeholder="" value="{{Request::old('Title')}}" >
+                                  <label for="Title">Title'</label>
+                                  <input type="text"  placeholder="Please enter data"   class="form-control"   name="Title"  id="Title" placeholder="" value="" >
                                   @if ($errors->has('Title'))
                                   <div class="invalid-feedback">
                                     {{ $errors->first('Title') }}
