57903f07fd1c75245144fa690a59e094d5ab6868
freejob985 <freejob985@gmail.com>
2021-01-16 12:48:01 +0200

    asd

--- resources/views/admin/service/service/edit.blade.php
+++ resources/views/admin/service/service/edit.blade.php
@@ -246,7 +246,7 @@
 
                               <div class="form-row">
                                 <div class="col-md-12 mb-4">
-                                  <label for="paragraph">paragraph'</label>
+                                  <label for="paragraph">paragraph</label>
                                   <input type="text"  placeholder="Please enter data"   class="form-control"   name="paragraph"  id="paragraph" placeholder="" value="{{Request::old('paragraph')}}" >
                                   @if ($errors->has('paragraph'))
                                   <div class="invalid-feedback">
